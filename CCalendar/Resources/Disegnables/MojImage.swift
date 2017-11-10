0eb3da666efeb24aac58f0b6144bf3ad6dcf5e34
Zeljko Bilbija <info@bilbija.net>
2017-11-11 00:13:10 +0100

    Manje izmene grafike...boje i slicno

--- CCalendar/Resources/Disegnables/MojImage.swift
+++ CCalendar/Resources/Disegnables/MojImage.swift
@@ -58,5 +58,16 @@ class MojImage: UIImageView
         }
     }
     
+    var maskImageView = UIImageView()
+    
+    
+    @IBInspectable var maskImage: UIImage?
+        {
+        didSet{
+            maskImageView.image = maskImage
+            maskImageView.frame = bounds
+            mask = maskImageView
+        }
+    }
     
 }
